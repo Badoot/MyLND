0f5275158b4ccc4906c2124c01a8c677c540bf0e
Badoot <badoot@gmail.com>
2018-11-12 22:25:11 -0800

    shuffling some things around

--- src/get_data.py
+++ src/get_data.py
@@ -57,37 +57,14 @@ def get_set_debug_level(show, level_spec):
     return response
 
 
-# # # # # # # # # #
-#      Peers
-# # # # # # # # # #
-
-def get_peers():
-    response = APICall.stub.ListPeers(ln.ListPeersRequest())
+def get_fee_report():
+    response = APICall.stub.FeeReport(ln.FeeReportRequest())
     return response
 
 
-# # # # # # # # # #
-#    Channels
-# # # # # # # # # #
-
-def get_channels():
-    response = APICall.stub.ListChannels(ln.ListChannelsRequest())
-    return response
-
-
-def get_pending_channels():
-    response = APICall.stub.PendingChannels(ln.PendingChannelsRequest())
-    return response
-
-
-def get_wallet_balance():
-    response = APICall.stub.WalletBalance(ln.WalletBalanceRequest())
-    return response
-
-
-def get_channel_balance():
-    response = APICall.stub.ChannelBalance(ln.ChannelBalanceRequest())
-    return response
+# # # # # # # # # # # # # # #
+#   Lightning Network
+# # # # # # # # # # # # # # #
 
 
 def get_network_info():
@@ -100,43 +77,17 @@ def get_describe_graph():
     return response
 
 
-def get_closed_channels():
-    response = APICall.stub.ClosedChannels(ln.ClosedChannelsRequest())
-    return response
+# # # # # # # # # #
+#      Peers
+# # # # # # # # # #
 
-
-def get_transactions():
-    response = APICall.stub.GetTransactions(ln.GetTransactionsRequest())
-    return response
-
-
-def get_list_payments():
-    response = APICall.stub.ListPayments(ln.ListPaymentsRequest())
-    return response
-
-
-def get_delete_payments():
-    response = APICall.stub.DeleteAllPayments(ln.DeleteAllPaymentsRequest())
-    return response
-
-
-def get_channel_info(chan_id):
-    response = APICall.stub.GetChanInfo(ln.ChanInfoRequest(chan_id=chan_id))
+def get_peers():
+    response = APICall.stub.ListPeers(ln.ListPeersRequest())
     return response
 
 
 def get_node_info(pub_key):
     response = APICall.stub.GetNodeInfo(ln.NodeInfoRequest(pub_key=pub_key))
-    return response
-
-
-def get_new_address():
-    response = APICall.stub.NewAddress(ln.NewAddressRequest())
-    return response
-
-
-def get_fee_report():
-    response = APICall.stub.FeeReport(ln.FeeReportRequest())
     return response
 
 
@@ -156,23 +107,31 @@ def get_disconnect_peer(pub_key):
     return response
 
 
-def get_open_channel(node_pubkey, local_funding_amount=None, push_sat=None):
-    pubkey_bytes = codecs.decode(node_pubkey, 'hex')
-    request = ln.OpenChannelRequest(
-        node_pubkey=pubkey_bytes,
-        node_pubkey_string=node_pubkey,
-        local_funding_amount=int(local_funding_amount),
-        push_sat=int(push_sat)
-        # TODO
-        # Need to add option to create a private channel
-        # If I tried to include the option, every channel was private,
-        # even with multiple "private=False" in the different modules. The
-        # default is "private=False", so accepting that my code is shit and just
-        # forcing that for now.
-        #
-        # private=bool(private)
-    )
-    response = APICall.stub.OpenChannelSync(request)
+# # # # # # # # # #
+#    Channels
+# # # # # # # # # #
+
+def get_channels():
+    response = APICall.stub.ListChannels(ln.ListChannelsRequest())
+    return response
+
+
+def get_pending_channels():
+    response = APICall.stub.PendingChannels(ln.PendingChannelsRequest())
+    return response
+
+
+def get_channel_balance():
+    response = APICall.stub.ChannelBalance(ln.ChannelBalanceRequest())
+    return response
+
+
+def get_closed_channels():
+    response = APICall.stub.ClosedChannels(ln.ClosedChannelsRequest())
+    return response
+
+def get_channel_info(chan_id):
+    response = APICall.stub.GetChanInfo(ln.ChanInfoRequest(chan_id=chan_id))
     return response
 
 
@@ -202,32 +161,23 @@ def get_close_channel(funding_tx, output_index, force):
     return response
 
 
-def wallet_unlock(password):
-    request = ln.UnlockWalletRequest(wallet_password=password.encode())
-    response = APICall.wallet_stub.UnlockWallet(request)
+# # # # # # # # # # # # # # #
+#    On-chain Transactions
+# # # # # # # # # # # # # # #
+
+
+def get_wallet_balance():
+    response = APICall.stub.WalletBalance(ln.WalletBalanceRequest())
     return response
 
 
-def change_password(current_password, new_password):
-    request = ln.ChangePasswordRequest(
-            current_password=current_password.encode(),
-            new_password=new_password.encode()
-            )
-    response = APICall.wallet_stub.ChangePassword(request)
+def get_transactions():
+    response = APICall.stub.GetTransactions(ln.GetTransactionsRequest())
     return response
 
 
-def get_gen_seed():
-    request = ln.GenSeedRequest()
-    response = APICall.wallet_stub.GenSeed(request)
-    return response
-
-
-def get_create(wallet_password, cipher_seed_mnemonic):
-    request = ln.InitWalletRequest(
-        wallet_password=wallet_password,
-        cipher_seed_mnemonic=cipher_seed_mnemonic)
-    response = APICall.wallet_stub.InitWallet(request)
+def get_new_address():
+    response = APICall.stub.NewAddress(ln.NewAddressRequest())
     return response
 
 
@@ -237,6 +187,41 @@ def get_send_coins(addr, amount):
         amount=int(amount),
     )
     response = APICall.stub.SendCoins(request)
+    return response
+
+
+# # # # # # # # # # # # # # #
+#    Lightning Payments
+# # # # # # # # # # # # # # #
+
+
+def get_list_payments():
+    response = APICall.stub.ListPayments(ln.ListPaymentsRequest())
+    return response
+
+
+def get_delete_payments():
+    response = APICall.stub.DeleteAllPayments(ln.DeleteAllPaymentsRequest())
+    return response
+
+
+def get_open_channel(node_pubkey, local_funding_amount=None, push_sat=None):
+    pubkey_bytes = codecs.decode(node_pubkey, 'hex')
+    request = ln.OpenChannelRequest(
+        node_pubkey=pubkey_bytes,
+        node_pubkey_string=node_pubkey,
+        local_funding_amount=int(local_funding_amount),
+        push_sat=int(push_sat)
+        # TODO
+        # Need to add option to create a private channel
+        # If I tried to include the option, every channel was private,
+        # even with multiple "private=False" in the different modules. The
+        # default is "private=False", so accepting that my code is shit and just
+        # forcing that for now.
+        #
+        # private=bool(private)
+    )
+    response = APICall.stub.OpenChannelSync(request)
     return response
 
 
@@ -292,4 +277,41 @@ def get_query_route(pub_key, amount, num_routes):
     )
     response = APICall.stub.QueryRoutes(request)
     return response
+
+
+# # # # # # # # # # # # # # #
+#    Wallet Stub Stuff
+# # # # # # # # # # # # # # #
+
+
+def wallet_unlock(password):
+    request = ln.UnlockWalletRequest(wallet_password=password.encode())
+    response = APICall.wallet_stub.UnlockWallet(request)
+    return response
+
+
+def change_password(current_password, new_password):
+    request = ln.ChangePasswordRequest(
+            current_password=current_password.encode(),
+            new_password=new_password.encode()
+            )
+    response = APICall.wallet_stub.ChangePassword(request)
+    return response
+
+
+def get_gen_seed():
+    request = ln.GenSeedRequest()
+    response = APICall.wallet_stub.GenSeed(request)
+    return response
+
+
+def get_create(wallet_password, cipher_seed_mnemonic):
+    request = ln.InitWalletRequest(
+        wallet_password=wallet_password,
+        cipher_seed_mnemonic=cipher_seed_mnemonic)
+    response = APICall.wallet_stub.InitWallet(request)
+    return response
+
+
+
 
