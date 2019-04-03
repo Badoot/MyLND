73c24560b9dbf7e31e96bbfa8b6d55adfb3e9f49
root <badoot@gmail.com>
2019-04-02 19:47:46 -0700

    added channel_point back into --listchannels

--- out_data.py
+++ out_data.py
@@ -244,10 +244,10 @@ def out_list_channels():
             node_info = converters.response_to_dict(node_info)
             alias = node_info['node']['alias']
             # List of fields to include in the output
-            channel = [active, private, chan_id, alias, num_updates, capacity, local_balance, remote_balance, unsettled_balance, total_satoshis_received, total_satoshis_sent]
+            channel = [active, private, chan_id, channel_point, alias, num_updates, capacity, local_balance, remote_balance, unsettled_balance, total_satoshis_received, total_satoshis_sent]
             channel_list.append(channel)
         # Build the DataFrame from list of channels
-        channels_df_columns = ['Active', 'Private', 'Channel ID', 'Remote Alias', 'Updates', 'Capacity', 'Local Balance', 'Remote Balance', 'Unsettled', 'Sats Received', 'Sats Sent']
+        channels_df_columns = ['Active', 'Private', 'Channel ID', 'Channel Point', 'Remote Alias', 'Updates', 'Capacity', 'Local Balance', 'Remote Balance', 'Unsettled', 'Sats Received', 'Sats Sent']
         channels_df = pd.DataFrame.from_records(channel_list, columns=channels_df_columns).to_string(index=False)
         # Print it
         print("\nChannels: " + str(len(channel_list)) + " total \n" + "-" * 18)
